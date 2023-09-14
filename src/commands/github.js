45b9e6aa9642b990649f40d4298ca9a686209078
Joshua Shen <joshausneh@gmail.com>
2023-09-14 14:46:19 -0500

    opens links on command submit

--- src/commands/github.js
+++ src/commands/github.js
@@ -4,11 +4,13 @@ import { CommandLine } from "./command"
 export const Github = (props) => {
     const command = props.command
 
+    window.open("https://github.com/josh-shen", "_blank")
+
     return (
         <div style={{paddingBottom: 15}}>
             <CommandLine command={command}></CommandLine>
             <br></br>
-            https://github.com/josh-shen
+            <a class="highlight" href="https://github.com/josh-shen">https://github.com/josh-shen</a>
         </div>
     )
 }
--- src/commands/linkedin.js
+++ src/commands/linkedin.js
@@ -4,11 +4,13 @@ import { CommandLine } from "./command"
 export const LinkedIn = (props) => {
     const command = props.command
 
+    window.open("https://www.linkedin.com/in/jo-shshen/", "_blank")
+    
     return (
         <div style={{paddingBottom: 15}}>
             <CommandLine command={command}></CommandLine>
             <br></br>
-            https://www.linkedin.com/in/jo-shshen/
+            <a class="highlight" href="https://www.linkedin.com/in/jo-shshen/">https://www.linkedin.com/in/jo-shshen/</a>
         </div>
     )
 }
